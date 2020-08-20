4b53943aab3690652a6a6ce9bca455648a9c56af
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2020-08-20 11:57:31 +0700

    Remove lodash

--- src/utils.ts
+++ src/utils.ts
@@ -1,19 +1,18 @@
 import { spawnSync } from 'child_process'
-import { compact } from 'lodash'
 import chalk from 'chalk'
 
 export const getEnvPairs = (content: string) => {
-  return compact(
-    content
-      .trim()
-      .split('\n')
-      .map((line) => (line.startsWith('#') ? null : line)),
-  ).map((line) => {
-    const separator = line.indexOf('=')
-    const key = line.substring(0, separator)
-    const value = line.substring(separator + 1)
-    return { key, value }
-  })
+  return content
+    .trim()
+    .split('\n')
+    .map((line) => (line.startsWith('#') ? null : line))
+    .filter((line) => line)
+    .map((line) => {
+      const separator = line!.indexOf('=')
+      const key = line!.substring(0, separator)
+      const value = line!.substring(separator + 1)
+      return { key, value }
+    })
 }
 
 export const updateEnv = (
